5a38256c6c172a9439b2ef06d64f7601eca8b719
Suraj Gupta <suraj9899gupta@gmail.com>
2026-06-30 23:05:03 +0530

    Completed SQL KPI analysis queries

--- SQL/Business Analysis.sql
+++ SQL/Business Analysis.sql
@@ -168,3 +168,134 @@ from (
     group by customer_id
     having count(order_id) > 1
 ) t;
+
+-- New Customers per Month
+
+select
+    year(first_order_date) as order_year,
+    month(first_order_date) as order_month,
+    count(*) as new_customers
+from (
+    select
+        customer_id,
+        min(order_purchase_timestamp) as first_order_date
+    from orders
+    group by customer_id
+) t
+group by
+    year(first_order_date),
+    month(first_order_date)
+order by
+    order_year,
+    order_month;
+    
+-- -------------------------Products--------------------------------
+
+-- Top Selling Products
+
+select
+    p.product_id,
+    p.product_category_name,
+    round(sum(o.price),2) as product_revenue
+from products p
+join order_items o
+on p.product_id = o.product_id
+group by
+    p.product_id,
+    p.product_category_name
+order by product_revenue desc
+limit 10;
+
+-- Top Categories
+select
+    p.product_category_name,
+    round(sum(o.price),2) as category_revenue
+from products p
+join order_items o
+on p.product_id = o.product_id
+group by p.product_category_name
+order by category_revenue desc
+limit 10;
+
+-- Average Product Rating
+select p.product_category_name, round(avg(r.review_score), 2) as avg_product_rating
+from products p
+join order_items o
+on p.product_id = o.product_id
+join reviews r
+on o.order_id = r.order_id
+group by p.product_category_name
+order by avg_product_rating desc;
+
+-- ---------------------------------Sellers--------------------------------
+
+-- Number of Sellers
+
+select count(distinct(seller_id)) 
+as no_of_sellers from sellers;
+
+-- Top Sellers by Revenue
+
+select s.seller_id, round(sum(oi.price), 2) as seller_revenue
+from sellers s
+join order_items oi
+on s.seller_id = oi.seller_id
+group by s.seller_id
+order by seller_revenue desc
+limit 10;
+
+-- ---------------------------------Delivery--------------------------------
+
+-- Average Delivery Time
+
+select
+    round(avg(datediff(order_delivered_customer_date,
+                       order_purchase_timestamp)), 2) as avg_delivery_days
+from orders
+where order_delivered_customer_date is not null;
+
+-- Delayed Deliveries
+
+select
+    order_id,
+    order_estimated_delivery_date,
+    order_delivered_customer_date,
+    datediff(order_delivered_customer_date,
+             order_estimated_delivery_date) as delayed_by_days
+from orders
+where order_delivered_customer_date > order_estimated_delivery_date;
+
+-- ----------------------------------Reviews---------------------------------
+
+-- Average Rating
+
+select avg(review_score) as avg_rating from reviews;
+
+-- Rating Distribution
+
+select
+    review_score,
+    count(*) as total_reviews
+from reviews
+group by review_score
+order by review_score;
+
+-- ---------------------------------Payments------------------------------------
+
+-- Most Used Payment Method
+
+select
+payment_type,
+count(*) as use_times
+from payments
+group by payment_type
+order by use_times desc;
+
+-- Installment Usage
+
+select
+payment_installments,
+count(*) as total_payments
+from payments
+group by payment_installments
+order by payment_installments;
